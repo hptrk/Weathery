e01b85c27288b78a721c72bcf55921181b8da191
Horánszki Patrik <horanszkipatrik@gmail.com>
2023-03-05 23:23:02 +0100

    finish the styling of the graph (+package update)

--- js/chart.js
+++ js/chart.js
@@ -1,24 +1,53 @@
-import Chart, { LinearScale } from "chart.js/auto"; //NOT AUTO // todo
+import Chart from "chart.js/auto"; //NOT AUTO // todo
 Chart.defaults.font.size = 14;
 Chart.defaults.font.family = "Red Hat Display, sans-serif";
-Chart.defaults.font.weight = 700;
-Chart.defaults.font.lineHeight = 1; //MIGHT CHANGE LATER // todo
+Chart.defaults.font.weight = 600;
+Chart.defaults.font.lineHeight = 1;
+Chart.defaults.elements.bar.backgroundColor = "red";
 
 let delayed;
 
 (async function () {
   const data = [
     { time: 10, chance: 50 },
-    { time: 11, chance: 30 },
+    { time: 11, chance: 40 },
     { time: 12, chance: 80 },
     { time: 1, chance: 20 },
-    { time: 2, chance: 40 },
-    { time: 3, chance: 30 },
+    { time: 2, chance: 65 },
+    { time: 3, chance: 50 },
+    { time: 2, chance: 90 },
   ];
 
   new Chart(document.querySelector(".diagram__container"), {
     type: "bar",
     options: {
+      scales: {
+        x: {
+          border: { display: false },
+          grid: {
+            lineWidth: 0,
+            tickLength: 0,
+          },
+          ticks: {
+            color: "#888",
+          },
+        },
+        y: {
+          border: { display: false },
+          max: 100,
+          min: 0,
+          position: "left",
+          grid: {
+            lineWidth: 1.5,
+            tickLength: 0,
+            color: "#242426",
+          },
+          ticks: {
+            color: "#888",
+            count: 6, //100, 80, 60, 40, 20, 0
+          },
+        },
+      },
       responsive: true,
       animation: {
         duration: 1000,
@@ -56,6 +85,10 @@ let delayed;
         {
           data: data.map((row) => row.chance),
           backgroundColor: "#bbd8ec",
+          hoverBackgroundColor: "#96adbd",
+          borderRadius: 100,
+          barThickness: 12,
+          borderSkipped: false,
         },
       ],
     },
