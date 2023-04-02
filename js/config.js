c0acbb33de0d47533593aecfc8377762aa9db4fd
Horánszki Patrik <horanszkipatrik@gmail.com>
2023-04-02 18:31:16 +0200

    expand the open-meteo API url with the hourly datas

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 export const API_URL = function (lat, long) {
-  return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=relativehumidity_2m,apparent_temperature&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`;
+  return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`;
 };
 export const TIMEOUT_SEC = 10; // Reject a promise after X seconds
