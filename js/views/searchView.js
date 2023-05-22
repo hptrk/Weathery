47ac3df9f9dbe9ec6a061a82d1bd7e5cd6231089
Horánszki Patrik <horanszkipatrik@gmail.com>
2023-05-22 18:18:55 +0200

    refactor searchView for better code readibility

--- js/views/searchView.js
+++ js/views/searchView.js
@@ -9,10 +9,14 @@ class SearchView extends View {
   _parentElement = document.querySelector('.navigation__searchbar-results');
   _searchBar = document.querySelector('.navigation__searchbar');
   _inputField = document.querySelector('.navigation__searchbar-input');
+  _results;
+  _favorites;
+  _borderHider = '<div class="border-hider"></div>'; // with a height of 5rem (hides the bottom borders)
   _errorMessage =
     'Something went wrong while loading your search results. Please refresh the page and try again.';
 
   addHandlerRender(render) {
+    // Write in the input field
     this._inputField.addEventListener('input', async () => {
       this._inputField.value.length >= 3 && render(); // if the input field has at least 3 characters
       if (this._inputField.value.length < 3) {
@@ -23,9 +27,11 @@ class SearchView extends View {
   }
 
   addBasicConfig() {
+    // Submitting input field
     this._searchBar.addEventListener('submit', e => {
       e.preventDefault(); // prevent default submitting
     });
+
     // when something in the form element is clicked, focus the input
     this._searchBar.addEventListener('click', () => this._inputField.focus());
 
@@ -68,42 +74,59 @@ class SearchView extends View {
   }
 
   generateResults(data, favorites) {
-    this._removeBoxes(); // remove previous data
+    // Remove previous data
+    this._removeBoxes();
     this._removeInnerHTML();
 
-    const resultsNumber = data.length; // this is how many results should be displayed
-    const borderHider = '<div class="border-hider"></div>'; // with a height of 5rem (hides the bottom borders)
+    this._results = data;
+    this._favorites = favorites;
 
-    const result = i => `
+    this._parentElement.innerHTML += this._borderHider; // add the bottom border filler
+
+    // Display search results markup
+    this._displaySearchResults();
+
+    // Handle like animation
+    this._likeAnimation();
+
+    // Handle element's style and animation
+    this._handleStyleAnimations();
+
+    this._preventFocusLoss(this._results.length); // without this prevention the boxes would be removed too fast
+  }
+
+  _searchResultMarkup(i) {
+    return `
 <div class="navigation__searchbar-results--box">
   ${city}
 
-  <span class="results-city">${data[i].city}</span>
-  <span class="results-country">${data[i].country}</span>
+  <span class="results-city">${this._results[i].city}</span>
+  <span class="results-country">${this._results[i].country}</span>
 
   ${
     // if the city is in the favorites list already, display the filled heart icon
-    favorites.some(obj => obj.lat === data[i].lat && obj.lon === data[i].lon)
+    this._favorites.some(
+      obj =>
+        obj.lat === this._results[i].lat && obj.lon === this._results[i].lon
+    )
       ? liked
       : like
   }
 </div>
 `;
+  }
 
-    this._parentElement.innerHTML += borderHider; // add the bottom border filler
-
-    // this forEach runs the result() function 'resultsNumber' times
-    Array.from({ length: resultsNumber }).forEach((_, i) => {
-      this._parentElement.innerHTML += result(i);
+  _displaySearchResults() {
+    // this forEach runs the _searchResultMarkup() function 'results.length' times
+    Array.from({ length: this._results.length }).forEach((_, i) => {
+      this._parentElement.innerHTML += this._searchResultMarkup(i);
     });
+  }
 
-    this._likeAnimation();
-
+  _handleStyleAnimations() {
     // add HEIGHT for animation
-    this._parentElement.style.height = resultsNumber * 5 + 5 + 'rem'; // *5 because 1 box is 5rem, +5 because of border hider
+    this._parentElement.style.height = this._results.length * 5 + 5 + 'rem'; // *5 because 1 box is 5rem, +5 because of border hider
     this._parentElement.style.boxShadow = '0 2.1rem 2rem rgba(0, 0, 0, 0.3)'; // animating the boxshadow
-
-    this._preventFocusLoss(resultsNumber); // without this prevention the boxes would be removed too fast
   }
 }
 export default new SearchView();
